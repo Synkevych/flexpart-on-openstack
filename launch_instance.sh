318863744930ae9132380a6d4751748c906f4555
Roman <sunkevu4@gmail.com>
2020-09-24 14:16:22 +0300

    Update launch_instance.sh

--- launch_instance.sh
+++ launch_instance.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Checking the number of arguments.
 if [ $# -ne 1 ]; then echo "Usage: $0 VM_name"; exit 1; fi
 
+# Assigning the first passed parameter to a constant VMNAME
 VMNAME=$1
 
+# go to HOME and Passing openstack Variables to Environment Variables(needed to launch an instance)
 cd
 . WRF-UNG.rc
 
+# try to start a new virtual machine
 while true; do
    nova boot --flavor=m1.medium --block-device source=image,id=d9657d95-4800-485b-b082-3e74efe17e9f,dest=volume,size=4,device=/dev/vda,bus=virtio,shutdown=remove,bootindex=0 --key-name cloud_key --security-group "WRF group" $VMNAME 2>/dev/null
    for i in `seq 1 3`; do
