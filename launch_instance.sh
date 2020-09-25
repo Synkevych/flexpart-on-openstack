e730d8da1e849f752e7f73de22d66ddd12a52eae
Roman <sunkevu4@gmail.com>
2020-09-25 16:37:09 +0300

    Update launch_instance.sh

--- launch_instance.sh
+++ launch_instance.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# Checking the number of arguments.
-if [ $# -ne 1 ]; then echo "Usage: $0 VM_name"; exit 1; fi
-
-# Assigning the first passed parameter to a constant VMNAME
-VMNAME=$1
+# generate random name for VMNAME
+VMNAME=vm_`date --utc +%Y%m%d%H%M%S`
+TIMER=60
 
 # go to HOME and Passing openstack Variables to Environment Variables(needed to launch an instance)
 cd
 . WRF-UNG.rc
 
 # try to start a new virtual machine
-while true; do
-   nova boot --flavor=m1.medium --block-device source=image,id=d9657d95-4800-485b-b082-3e74efe17e9f,dest=volume,size=4,device=/dev/vda,bus=virtio,shutdown=remove,bootindex=0 --key-name cloud_key --security-group "WRF group" $VMNAME 2>/dev/null
+nova boot --flavor 3 --image 5c4ceaf0-2d65-46a0-b62d-4be163804809 --key-name cloud_key --security-groups d134acb2-e6bc-4c82-a294-9617fdf7bf07 --user-data $home/helloworld.sh $VMNAME 2>/dev/null
    for i in `seq 1 3`; do
-     sleep 60
+      sec=$TIMER
+      while [ $sec -ge 0 ]; do
+	      echo -ne "$i attempt to start VM: $sec\033[0K\r"
+              let "sec=sec-1"
+              sleep 1
+      done
      STATUS=`openstack server list | grep $VMNAME | awk '{ print $6 }'`
+     echo 'VM status is - $STATUS'
      if [ "x$STATUS" = "xACTIVE" ]; then
-       echo "STATUS is ACTIVE"
+       echo "VM $VMNAME is ACTIVE"
        exit
      fi
    done
-   openstack server delete `openstack server list | grep $VMNAME | awk
-'{ print $2 }'`
+   openstack server delete `openstack server list | grep $VMNAME | awk '{ print $2 }'`
 done
