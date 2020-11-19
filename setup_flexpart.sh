52cd9dd2aabfb5ea3dbade6b073199a043ee29f3
Roman <sunkevu4@gmail.com>
2020-11-19 15:32:50 +0200

    Update setup_flexpart.sh

--- setup_flexpart.sh
+++ setup_flexpart.sh
@@ -14,36 +14,32 @@ export FC=gfortran
 # jasper
 
 cd /home/ubuntu
-mkdir flexpart & cd flexpart
+mkdir flexpart ; cd flexpart
 echo "starting istallation flexpart" >> installation.log
 
+# here you can change the default install location
 export DIR=/home/ubuntu/flexpart
 
-wget https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
-unzip jasper-1.900.1.zip
-rm jasper-1.900.1.zip
+wget https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip ;
+unzip jasper-1.900.1.zip ; rm jasper-1.900.1.zip
 cd jasper-1.900.1
 chmod +x configure
-./configure --prefix=/home/ubuntu/flexpart
-make clean
-make -j
-make -j check
+./configure --prefix=$DIR
+make clean ; make -j ; make -j check
 make install
 
 cd /home/ubuntu/flexpart
 echo "jasper istalled" >> installation.log
 rm -rf jasper-1.900.1
+
 # grib_api
 
-wget https://people.freebsd.org/~sunpoet/sunpoet/grib_api-1.26.1-Source.tar.gz
-tar -xvf grib_api-1.26.1-Source.tar.gz
-rm grib_api-1.26.1-Source.tar.gz
+wget https://people.freebsd.org/~sunpoet/sunpoet/grib_api-1.26.1-Source.tar.gz ;
+tar -xvf grib_api-1.26.1-Source.tar.gz ; rm grib_api-1.26.1-Source.tar.gz
 cd grib_api-1.26.1-Source
 chmod +x configure
-./configure --prefix=/home/ubuntu/flexpart --with-jasper=/home/ubuntu/flexpart --disable-shared
-make clean
-make -j
-make -j check
+./configure --prefix=$DIR --with-jasper=$DIR --disable-shared
+make clean ; make -j ; make -j check
 make install
 
 cd /home/ubuntu/flexpart
@@ -52,15 +48,12 @@ rm -rf grib_api-1.26.1-Source
 
 # zlib
 
-wget https://zlib.net/fossils/zlib-1.2.11.tar.gz
-tar -zxvf zlib-1.2.11.tar.gz
-rm zlib-1.2.11.tar.gz
+wget https://zlib.net/fossils/zlib-1.2.11.tar.gz ;
+tar -zxvf zlib-1.2.11.tar.gz ; rm zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 chmod +x configure
-./configure --prefix=/home/ubuntu/flexpart
-make clean
-make -j
-make -j check
+./configure --prefix=$DIR
+make clean ; make -j ; make -j check
 make install
 
 cd /home/ubuntu/flexpart
@@ -69,15 +62,12 @@ rm -rf zlib-1.2.11
 
 # hdf5
 
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.17/src/hdf5-1.8.17.tar.gz
-tar -zxvf hdf5-1.8.17.tar.gz
-rm hdf5-1.8.17.tar.gz
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.17/src/hdf5-1.8.17.tar.gz ;
+tar -zxvf hdf5-1.8.17.tar.gz ; rm hdf5-1.8.17.tar.gz
 cd hdf5-1.8.17/
 chmod +x configure
-./configure --prefix=/home/ubuntu/flexpart --with-zlib=/home/ubuntu/flexpart
-make clean
-make -j
-make check
+./configure --prefix=$DIR --with-zlib=$DIR
+make clean ; make -j ; make check
 make install
 
 cd /home/ubuntu/flexpart
@@ -86,15 +76,14 @@ rm -rf hdf5-1.8.17
 
 # netcdf
 
-wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.6.1.tar.gz
-tar -zxvf netcdf-4.6.1.tar.gz
-rm netcdf-4.6.1.tar.gz
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.6.1.tar.gz ;
+tar -zxvf netcdf-4.6.1.tar.gz ; rm netcdf-4.6.1.tar.gz
 cd  netcdf-4.6.1
 chmod +x configure
-./configure --prefix=/home/ubuntu/flexpart CPPFLAGS="-I/home/ubuntu/flexpart/include -O" LDFLAGS="-L/home/ubuntu/flexpart/lib" --enable-shared --enable-netcdf-4 --disable-dap --disable-doxygen
-make clean
-make -j
-make -j check
+./configure --prefix=$DIR \
+CPPFLAGS="-I$DIR/include -O" \
+LDFLAGS="-L$DIR/lib" --enable-shared --enable-netcdf-4 --disable-dap --disable-doxygen
+make clean ; make -j ; make -j check
 make install
 
 cd /home/ubuntu/flexpart
@@ -103,16 +92,14 @@ rm -rf netcdf-4.6.1
 
 # netcd-fortran
 
-wget https://www.gfd-dennou.org/arch/netcdf/unidata-mirror/netcdf-fortran-4.4.5.tar.gz
-tar -zxvf netcdf-fortran-4.4.5.tar.gz
-rm netcdf-fortran-4.4.5.tar.gz
+wget https://www.gfd-dennou.org/arch/netcdf/unidata-mirror/netcdf-fortran-4.4.5.tar.gz ;
+tar -zxvf netcdf-fortran-4.4.5.tar.gz ; rm netcdf-fortran-4.4.5.tar.gz
 cd  netcdf-fortran-4.4.5
 chmod +x configure
-./configure --prefix=/home/ubuntu/flexpart CPPFLAGS="-I/home/ubuntu/flexpart/include -O" LDFLAGS="-L/home/ubuntu/flexpart/lib" --enable-shared --enable-netcdf-4 --disable-dap --disable-doxygen
-make clean
-make -j
-make check
-make check
+./configure --prefix=$DIR \
+CPPFLAGS="-I$DIR/include -O" \
+LDFLAGS="-L$DIR/lib" --enable-shared --disable-doxygen
+make clean ; make -j ; make check ; make check # on first launch, one test fails
 make install
 
 cd /home/ubuntu/flexpart
