06b9a2a91433f3829b07518f49c0f85b510ab20f
Synkevych <sunkevu4@gmail.com>
2020-12-01 18:25:04 +0200

    update: download_grib.sh script

--- downloads_grib.sh
+++ downloads_grib.sh
@@ -1,27 +1,67 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then echo "Usage: $0 YYYYMMDD, Date_To"; exit 1; fi
+if [ $# -ne 2 ]; then echo "Usage: $0 From, To YYYYMMDD YYYYMMDD"; exit 1; fi
 
-DATE_FROM=$1
-YEAR=`echo $DATE_FROM | awk -F\- '{print $1}'`
-MONTH=`echo $DATE_FROM | awk -F\- '{print $2}'`
-DAY=`echo $DATE_FROM | awk -F\- '{print $3}'`
+FROM_YY=$(date -d "$1" '+%Y'); FROM_MM=$(date -d "$1" '+%m'); FROM_DD=$(date -d "$1" '+%d')
+TO_YEAR=$(date -d "$2" '+%Y'); TO_MONTH=$(date -d "$2" '+%m'); TO_DAY=$(date -d "$2" '+%d')
 
-DATE_TO=$2
-HOUR=(0000 0000 0600 1200 1800)
-MINUTE=(000 006 006 006 006)
+AMOUNT_OF_DAYS=$((10#$TO_DAY - 10#$DAY))
+HOUR=(0000 0600 1200 1800 0000)
 
-counter=$((DATE_TO/5))
-day_counter=$((DATE_TO-counter*5))
+# if YYMM <= 7 month from this date use first link else historical
+now=$(date); active_date=$(date --date="$now -7 month" +%s); date_from=$(date --date="$1" +%s)
+if [ $date_from -ge $active_date ]; then
+        DOMAIN="https://www.ncei.noaa.gov/data/global-forecast-system/access/grid-004-0.5-degree/analysis/"
+        PREFIX="gfs_4_"
+else
+        DOMAIN="https://www.ncei.noaa.gov/data/global-forecast-system/access/historical/analysis/"
+        PREFIX="gfsanl_4_"
+fi
 
-for i in `seq 0 $counter`; do
-  for j in `seq 0 4`; do
-   echo "day $(($MONTH+i))"
-  # echo "gfs_4_"${YEAR}${MONTH}$(($DAY+$i))"_"${HOUR[j]}"_"${MINUTE[j]}".grb2"
-  done
-  if [ "$i" -eq "$counter" ] && [ "$day_counter" -gt 0 ]; then
-    for j in `seq 0 $day_counter`; do
-     echo "gfs_4_"${YEAR}${MONTH}$(($DAY+i+1))"_"${HOUR[j]}"_"${MINUTE[j]}".grb2"
-    done
-  exit 1; fi
+# if folder does not exist create them
+[ -d grib_data ] || mkdir grib_data
+
+for i in `seq 0 $AMOUNT_OF_DAYS`; do
+	# if day less than 10 add 0 before
+	YYMMDD="${FROM_YY}${FROM_MM}${FROM_DD}"
+
+        # at first step download 0000_000 forecast
+        if [ "$i" -eq 0 ]; then
+                FILE=${PREFIX}${YYMMDD}"_0000_000.grb2"
+                URL="${DOMAIN}${FROM_YY}${FROM_MM}/${YYMMDD}"
+
+                wget -c "${URL}/${FILE}" -P "grib_data"
+                echo "${URL}/${FILE}" >> grib_data/file_for_downloads.txt
+                echo "${YYMMDD} 000000      ${FILE}     ON DISC" >> AVAILABLE
+
+		# change COMMAND/RELEASES start date
+		sed -i "s/STRYMMDD/${YYMMDD}/g" options/COMMAND
+		sed -i "s/STRYMMDD/${YYMMDD}/g" options/RELEASES
+        fi
+        # here we need test date, it is exist in this month or not
+        date=$(date --date="$1 + $i day");
+        FROM_YY=$(date -d "$date" '+%Y'); FROM_MM=$(date -d "$date" '+%m'); FROM_DD=$(date -d "$date" '+%d')
+        YYMMDD="${FROM_YY}${FROM_MM}${FROM_DD}"
+        # download 4 forecasts for every day
+
+        for j in `seq 0 3`; do
+                # if hour 1800 increment day
+		PR_DD=${FROM_DD}
+		if [ "$j" -eq 3 ]; then
+                        PR_DD=$(date -d "$date +1" '+%d')
+		fi
+
+                HH="${HOUR[j]}"
+		iterator=$((j+1))
+		PR_HH="${HOUR[iterator]}"
+                FILE=${PREFIX}${YYMMDD}"_"${HH}"_006.grb2"
+                URL="${DOMAIN}${FROM_YY}${FROM_MM}/${YYMMDD}"
+
+                wget -c "${URL}/${FILE}" -P "grib_data"
+                echo "${URL}/${FILE}" >> grib_data/file_for_downloads.txt
+                echo "${FROM_YY}${FROM_MM}${PR_DD} ${PR_HH}00      ${FILE}     ON DISC" >> AVAILABLE
+        done
 done
+
+sed -i "s/ENDYMMDD/${YYMMDD}/g" options/COMMAND
+sed -i "s/ENDYMMDD/${YYMMDD}/g" options/RELEASES
