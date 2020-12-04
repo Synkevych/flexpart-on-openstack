a28f2c819c4a3fa7ed3c190401bf621458e62637
Synkevych <sunkevu4@gmail.com>
2020-12-04 10:09:09 +0200

    fix: day for prognosis 1800 hour

--- downloads_grib.sh
+++ downloads_grib.sh
@@ -6,7 +6,7 @@ FROM_YY=$(date -d "$1" '+%Y'); FROM_MM=$(date -d "$1" '+%m'); FROM_DD=$(date -d 
 TO_YEAR=$(date -d "$2" '+%Y'); TO_MONTH=$(date -d "$2" '+%m'); TO_DAY=$(date -d "$2" '+%d')
 
 FROM_to_sec=$(date --date="$1" +%s); TO_to_sec=$(date --date="$2" +%s)
-AMOUNT_OF_DAYS=$(( (TO_to_sec- FROM_to_sec)/86400))
+AMOUNT_OF_DAYS=$(( (TO_to_sec - FROM_to_sec)/86400))
 
 HOUR=(0000 0600 1200 1800 0000)
 URL="https://www.ncei.noaa.gov/data/global-forecast-system/access/"
@@ -34,7 +34,7 @@ for i in `seq 0 $AMOUNT_OF_DAYS`; do
                 FILE=${PREFIX}${YYMMDD}"_0000_000.grb2"
                 URL="${DOMAIN}${FROM_YY}${FROM_MM}/${YYMMDD}"
 
-                # wget -c "${URL}/${FILE}" -P "grib_data"
+                wget -c "${URL}/${FILE}" -P "grib_data"
                 echo "${URL}/${FILE}" >> grib_data/file_for_downloads.txt
                 echo "${YYMMDD} 000000      ${FILE}     ON DISC" >> AVAILABLE
 
@@ -52,7 +52,8 @@ for i in `seq 0 $AMOUNT_OF_DAYS`; do
                 # if hour 1800 increment day
 		PR_DD=${FROM_DD}
 		if [ "$j" -eq 3 ]; then
-                        PR_DD=$(date -d "$date +1" '+%d')
+                        PR_date=$(date -d "$date + 1 day")
+                        PR_DD=$(date -d "$PR_date" '+%d')
 		fi
 
                 HH="${HOUR[j]}"
@@ -61,7 +62,7 @@ for i in `seq 0 $AMOUNT_OF_DAYS`; do
                 FILE=${PREFIX}${YYMMDD}"_"${HH}"_006.grb2"
                 URL="${DOMAIN}${FROM_YY}${FROM_MM}/${YYMMDD}"
 
-                # wget -c "${URL}/${FILE}" -P "grib_data"
+                wget -c "${URL}/${FILE}" -P "grib_data"
                 echo "${URL}/${FILE}" >> grib_data/file_for_downloads.txt
                 echo "${FROM_YY}${FROM_MM}${PR_DD} ${PR_HH}00      ${FILE}     ON DISC" >> AVAILABLE
         done
