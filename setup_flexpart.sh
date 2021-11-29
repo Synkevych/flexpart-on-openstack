a80827b067265c06d128d0c7a8bed0da42a7b19a
Roman <sunkevu4@gmail.com>
2021-11-30 01:26:40 +0200

    Update setup_flexpart.sh

--- setup_flexpart.sh
+++ setup_flexpart.sh
@@ -13,11 +13,10 @@ export FC=gfortran
 
 # jasper
 
-mkdir flexpart_lib ; cd flexpart_lib
-echo "starting istallation flexpart" >> installation.log
-
 # here you can change the default install location
 export DIR=$(pwd)
+
+echo "starting istallation flexpart" >> installation.log
 
 # dowload all libs from env.kiev.ua
 wget http://env.com.ua/~sunkevu4/flexpart/all_lib.tgz
